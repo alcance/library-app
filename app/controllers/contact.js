cd06c492612e4a88990c702c38f215d3dbe7e9ce
Iván Martínez <alcance.enlinea@gmail.com>
2016-07-28 20:16:44 -0700

    Sending message in contact form

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  emailAddress: '',
+  message: '',
+
+  // Form Validation: Email and message.
+  isEmailValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isMessageLongEnough: Ember.computed.gte('message.length', 5),
+
+  isValid: Ember.computed.and('isEmailValid', 'isMessageLongEnough'),
+
+  actions: {
+    sendMessage() {
+
+      var email = this.get('emailAddress');
+      var message = this.get('message');
+
+      alert('Sending message');
+
+      var responseMessage = 'To: ' + email + ', Message: ' + message;
+      console.log(responseMessage);
+      this.set('responseMessage', responseMessage);
+
+      //clear properties
+      this.set('emailAddress', '');
+      this.set('message', '');
+
+    }
+  }
+
 });
