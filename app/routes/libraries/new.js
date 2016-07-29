5c47a8cb59ee8c2d08a36da20d70b4fc49775aee
Iván Martínez <alcance.enlinea@gmail.com>
2016-07-28 22:20:20 -0700

    Contact controller and added willTransition method

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -8,6 +8,11 @@ export default Ember.Route.extend({
   actions: {
     saveLibrary(newLibrary) {
       newLibrary.save().then(() => this.transitionTo('libraries'));
+    },
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').rollbackAttributes();
     }
   }
 
