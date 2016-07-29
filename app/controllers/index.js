efd6849ff07905e1c71281a28b98e8269ed0077b
Iván Martínez <alcance.enlinea@gmail.com>
2016-07-28 20:25:03 -0700

    Saving record in controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,19 +3,29 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   emailAddress: '',
-  isDisabled: true,
+  responseMessage: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isDisabled: Ember.computed.not('isValid'),
 
   actions: {
     saveInvitation() {
-      alert(`Saving of the following address in progress:
-        ${this.get('emailAddress')}`);
+      // get emailAddress value
+      const email = this.get('emailAddress');
 
+      // Create invitation record
+      const newInvitation = this.store.createRecord('invitation', {
+        email: email
+      });
+
+      // Save record
+      newInvitation.save();
+
+      // Set response message
       this.set('responseMessage', `Thank you! We have just saved your email
         address: ${this.get('emailAddress')}`);
 
+      // Clear emailAddress prop
       this.set('emailAddress', '');
     }
   }
